99b49e3a38ac7598d11a35a992760036e554506e
Andrei Avram <avramandrei@ymail.com>
2019-01-30 23:40:05 +0200

    Move schema to file.

--- test/conditions.js
+++ test/conditions.js
@@ -1,34 +1,13 @@
 'use strict';
 
+const fs = require('fs');
 const validate = require('jsonschema').validate;
-
 const expect = require('chai').expect;
 
 const client = require('..');
 const apixu = new client.Apixu();
 
-const schema = {
-	"type": "array",
-	"minItems": 1,
-	"items": {
-		"type": "object",
-		"required": ["code", "day", "night", "icon"],
-		"properties": {
-			"code" : {
-				"type": "number",
-			},
-			"day" : {
-				"type": "string",
-			},
-			"night" : {
-				"type": "string",
-			},
-			"icon" : {
-				"type": "number",
-			},
-		},
-	},
-};
+const schema = fs.readFileSync(__dirname + '/conditions.json');
 
 describe('Conditions', () => {
 	it('it should retrieve weather conditions list', (done) => {
