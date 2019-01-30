4920cf6b399ca64a09dd29955592453810b64599
Andrei Avram <avramandrei@ymail.com>
2019-01-31 00:11:46 +0200

    Add forecast test.

---
+++ test/forecast.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const fs = require('fs');
+const validate = require('jsonschema').validate;
+const expect = require('chai').expect;
+
+const client = require('..');
+const config = client.config;
+
+config.apikey = process.env.APIXUKEY;
+const apixu = new client.Apixu(config);
+
+const schema = JSON.parse(fs.readFileSync(__dirname + '/forecast.json'));
+
+describe('Forecast', () => {
+	it('it should retrieve weather forecast by given query and number of days', (done) => {
+		apixu.forecast('London', 2).then((forecast) => {
+			const v = validate(forecast, schema);
+			expect(v.errors, v).to.have.length(0);
+			done();
+		}, (err) => {
+			console.log(err)
+		});
+	});
+});
