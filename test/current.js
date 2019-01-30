2bc2c2a8ecdc67ab16e0a534361a2b58ca01a981
Andrei Avram <avramandrei@ymail.com>
2019-01-30 23:42:12 +0200

    Add current test.

---
+++ test/current.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const fs = require('fs');
+const validate = require('jsonschema').validate;
+const expect = require('chai').expect;
+
+const client = require('..');
+const config = client.config;
+
+config.apikey = process.env.APIXUKEY;
+const apixu = new client.Apixu(config);
+
+const schema = fs.readFileSync(__dirname + '/current.json');
+
+describe('Current', () => {
+	it('it should retrieve current weather information by given query', (done) => {
+		apixu.current('London').then((current) => {
+			const v = validate(current, schema);
+			expect(v.errors, v).to.have.length(0);
+			done();
+		}, (err) => {
+			console.log(err)
+		});
+	});
+});
