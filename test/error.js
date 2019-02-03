03f5570ca932a406b6dd8bb2a952d4a5a00540b1
Andrei Avram <avramandrei@ymail.com>
2019-02-03 03:57:00 +0200

    Add errors.

--- test/error.js
+++ test/error.js
@@ -4,13 +4,14 @@
 const expect = require('chai').expect;
 
 const client = require('..');
+const errors = require('../errors');
 const apixu = new client.Apixu({});
 
 describe('Error', () => {
   it('it should return an error', (done) => {
     apixu.search('London').then(() => {}, (err) => {
       expect(err).to.be.an('error');
-      expect(err.code).to.be.eql(1002);
+      expect(err.code).to.be.eql(errors.API_KEY_NOT_PROVIDED);
       expect(err.message).to.have.lengthOf.greaterThan(0);
       done();
     });
